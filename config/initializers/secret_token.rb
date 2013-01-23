a8f9dc45ea1bf02d58baa08e92b63cdffe7c141f
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-01-23 13:44:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeSandbox1::Application.config.secret_token = '9e4063774afc3267fafeb457e76bcc38306d3b91f892d62e828153e70f3e312b9c8729a612483273b0f4cdf06770fda0803a3cc361582b8f57806d659b101207'
